d2562d4017c8a7487183435607efaddc1c576983
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-06-13 19:37:37 +0200

    Added 2 pages and css, changed directory names

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -3,7 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
+import Photography from "./pages/Photography";
 import Artist from "./pages/Artist";
+import Photographer from "./pages/Photographer";
 import About from "./pages/About";
 
 const router = createBrowserRouter([
@@ -20,8 +22,17 @@ const router = createBrowserRouter([
         element: <Gallery />,
       },
       {
+        path: "photography",
+        element: <Photography />,
+      },
+
+      {
         path: "artist",
         element: <Artist />,
+      },
+      {
+        path: "photographer",
+        element: <Photographer />,
       },
       {
         path: "about",
