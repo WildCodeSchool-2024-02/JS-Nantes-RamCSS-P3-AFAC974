4b6aeaf5b0e5b0e9ee83442d56d1e97e2f406cf8
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-07-09 14:00:09 +0200

    add animation for burger menu~

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,19 +9,14 @@ function Header() {
   const { user, SetUser } = useContext(UserConnectionContext);
 
   const [open, setOpen] = useState(0);
-  const [menuBurger, setMemuBurger] = useState(
-    "../images/images/menu-burger.png"
-  );
   const openMenu = () => {
     if (open === 0) {
       setOpen(1);
       document.querySelector("#vu-menu").style.display = "flex";
-      setMemuBurger("../images/images/menu-burger1.png");
     }
     if (open === 1) {
       setOpen(0);
       document.querySelector("#vu-menu").style.display = "none";
-      setMemuBurger("../images/images/menu-burger.png");
     }
   };
   const disconnect = () => {
@@ -42,9 +37,56 @@ function Header() {
             />
           </NavLink>
           <div>
-            <button id="img-menu" type="submit" onClick={openMenu}>
-              <img src={menuBurger} alt="ouvrir le menu" />
-            </button>
+            <svg
+              className="hb"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 10 10"
+              stroke="#6b6986"
+              fill="rgba(0,0,0,0)"
+              onClick={openMenu}
+            >
+              <path d="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7">
+                <animate
+                  dur="0.2s"
+                  attributeName="d"
+                  values="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7;M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7"
+                  fill="freeze"
+                  begin="start.begin"
+                />
+                <animate
+                  dur="0.2s"
+                  attributeName="d"
+                  values="M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7;M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7"
+                  fill="freeze"
+                  begin="reverse.begin"
+                />
+              </path>
+              <rect width="10" height="10" stroke="none">
+                <animate
+                  dur="2s"
+                  id="reverse"
+                  attributeName="width"
+                  begin="click"
+                />
+              </rect>
+              <rect width="10" height="10" stroke="none">
+                <animate
+                  dur="0.001s"
+                  id="start"
+                  attributeName="width"
+                  values="10;0"
+                  fill="freeze"
+                  begin="click"
+                />
+                <animate
+                  dur="0.001s"
+                  attributeName="width"
+                  values="0;10"
+                  fill="freeze"
+                  begin="reverse.begin"
+                />
+              </rect>
+            </svg>
             <ul id="vu-menu">
               <li>
                 <NavLink to="./" onClick={openMenu}>
