69b5d7676ad31afade08065a55dbab1d33381c01
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-07-11 20:18:55 +0200

    update database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -19,61 +19,61 @@ VALUES (
         "Emma",
         "Martin",
         "Spécialiste de la macrophotographie, Emma capture la vie miniature des récifs coralliens. Sa maîtrise des couleurs et des textures transporte le spectateur dans un monde invisible à l'œil nu, révélant la beauté délicate et complexe de chaque créature.",
-        "images/photos/photographer0/photographer.jpg"
+        "images/photos/photographer1/photographer.jpg"
     ),
     (
         "Lucas",
         "Dubois",
         "Lucas excelle dans la photographie de paysages sous-marins. Ses clichés captivent par leurs vastes étendues bleues, leurs jeux de lumière et l'immensité silencieuse des océans, invitant au voyage et à la contemplation.",
-        "images/photos/photographer1/photographer.jpg"
+        "images/photos/photographer2/photographer.jpg"
     ),
     (
         "Léa",
         "Leroy",
         "Expertise dans les portraits marins, Léa immortalise l'expression unique des animaux marins. Sa sensibilité artistique transparaît dans chaque cliché, révélant les émotions et la personnalité de ses sujets aquatiques avec une touche de poésie visuelle.",
-        "images/photos/photographer2/photographer.jpg"
+        "images/photos/photographer3/photographer.jpg"
     ),
     (
         "Hugo",
         "Lefèvre",
         "Spécialisé dans la photographie de la vie nocturne sous-marine. Ses images transmettent une atmosphère envoûtante, où la lumière naturelle se mêle à la fluorescence, révélant un monde magique et insoupçonner.",
-        "images/photos/photographer3/photographer.jpg"
+        "images/photos/photographer4/photographer.jpg"
     ),
     (
         "Camille",
         "Dupont",
         "Camille se distingue à travers ses photographies engagées, elle documente les défis écologiques et l'harmonie fragile entre l'homme et la mer, invitant à la réflexion et à l'action pour la préservation des océans.",
-        "images/photos/photographer4/photographer.jpg"
+        "images/photos/photographer5/photographer.jpg"
     ),
     (
         "Nathan",
         "Lambert",
         "Nathan capture la grâce et la puissance des grands prédateurs marins. Ses images saisissent l'instant précis où la nature sauvage et la beauté brute se rencontrent, offrant une perspective intime sur la vie marine.",
-        "images/photos/photographer5/photographer.jpg"
+        "images/photos/photographer6/photographer.jpg"
     ),
     (
         "Manon",
         "Girard",
         "Manon révèle les histoires cachées des fonds marins. À travers ses séries captivantes, elle témoigne de la diversité biologique et des enjeux de conservation, inspirant une connexion profonde avec l'océan.",
-        "images/photos/photographer6/photographer.jpg"
+        "images/photos/photographer7/photographer.jpg"
     ),
     (
         "Louis",
         "Morel",
         "Expert en photographie sous-marine, il crée des compositions captivantes qui transcendent la réalité. Ses œuvres fusionnent lumière, mouvement et texture pour exprimer une vision poétique de la vie sous-marine, éveillant l'imagination et les émotions.",
-        "images/photos/photographer7/photographer.jpg"
+        "images/photos/photographer8/photographer.jpg"
     ),
     (
         "Sarah",
         "Bernard",
         "Connu pour ses portraits intimistes de la faune marine, Sarah capture l'âme des créatures marines avec une sensibilité unique. À travers ses photographies pleines d'émotions, elle invite à découvrir la beauté et la diversité du monde sous-marin.",
-        "images/photos/photographer8/photographer.jpg"
+        "images/photos/photographer9/photographer.jpg"
     ),
     (
         "Alexandre",
         "Renault",
         "Alexandre se spécialise dans la photographie de sites sous-marins uniques et peu explorés. Ses images époustouflantes capturent la grandeur des formations géologiques sous-marines, révélant des paysages subaquatiques spectaculaires et méconnus.",
-        "images/photos/photographer9/photographer.jpg"
+        "images/photos/photographer10/photographer.jpg"
     );
 
 CREATE TABLE artwork (
@@ -84,8 +84,7 @@ CREATE TABLE artwork (
     alt_artwork VARCHAR(100) NULL,
     id_artist INT NOT NULL,
     PRIMARY KEY (`id`),
-        FOREIGN KEY (id_artist) REFERENCES artist(id)
-        ON DELETE CASCADE
+    FOREIGN KEY (id_artist) REFERENCES artist (id) ON DELETE CASCADE
 );
 
 INSERT INTO
@@ -95,106 +94,606 @@ INSERT INTO
         image,
         id_artist
     )
-VALUES ("ll", "ll", "photo1.jpg", 1),
-    ("ll", "ll", "photo2.jpg", 1),
-    ("ll", "ll", "photo3.jpg", 1),
-    ("ll", "ll", "photo4.jpg", 1),
-    ("ll", "ll", "photo5.jpg", 1),
-    ("ll", "ll", "photo6.jpg", 1),
-    ("ll", "ll", "photo7.jpg", 1),
-    ("ll", "ll", "photo8.jpg", 1),
-    ("ll", "ll", "photo9.jpg", 1),
-    ("ll", "ll", "photo10.jpg", 1),
-    ("ll", "ll", "photo1.jpg", 2),
-    ("ll", "ll", "photo2.jpg", 2),
-    ("ll", "ll", "photo3.jpg", 2),
-    ("ll", "ll", "photo4.jpg", 2),
-    ("ll", "ll", "photo5.jpg", 2),
-    ("ll", "ll", "photo6.jpg", 2),
-    ("ll", "ll", "photo7.jpg", 2),
-    ("ll", "ll", "photo8.jpg", 2),
-    ("ll", "ll", "photo9.jpg", 2),
-    ("ll", "ll", "photo10.jpg", 2),
-    ("ll", "ll", "photo1.jpg", 3),
-    ("ll", "ll", "photo2.jpg", 3),
-    ("ll", "ll", "photo3.jpg", 3),
-    ("ll", "ll", "photo4.jpg", 3),
-    ("ll", "ll", "photo5.jpg", 3),
-    ("ll", "ll", "photo6.jpg", 3),
-    ("ll", "ll", "photo7.jpg", 3),
-    ("ll", "ll", "photo8.jpg", 3),
-    ("ll", "ll", "photo9.jpg", 3),
-    ("ll", "ll", "photo10.jpg", 3),
-    ("ll", "ll", "photo1.jpg", 4),
-    ("ll", "ll", "photo2.jpg", 4),
-    ("ll", "ll", "photo3.jpg", 4),
-    ("ll", "ll", "photo4.jpg", 4),
-    ("ll", "ll", "photo5.jpg", 4),
-    ("ll", "ll", "photo6.jpg", 4),
-    ("ll", "ll", "photo7.jpg", 4),
-    ("ll", "ll", "photo8.jpg", 4),
-    ("ll", "ll", "photo9.jpg", 4),
-    ("ll", "ll", "photo10.jpg", 4),
-    ("ll", "ll", "photo1.jpg", 5),
-    ("ll", "ll", "photo2.jpg", 5),
-    ("ll", "ll", "photo3.jpg", 5),
-    ("ll", "ll", "photo4.jpg", 5),
-    ("ll", "ll", "photo5.jpg", 5),
-    ("ll", "ll", "photo6.jpg", 5),
-    ("ll", "ll", "photo7.jpg", 5),
-    ("ll", "ll", "photo8.jpg", 5),
-    ("ll", "ll", "photo9.jpg", 5),
-    ("ll", "ll", "photo10.jpg", 5),
-    ("ll", "ll", "photo1.jpg", 6),
-    ("ll", "ll", "photo2.jpg", 6),
-    ("ll", "ll", "photo3.jpg", 6),
-    ("ll", "ll", "photo4.jpg", 6),
-    ("ll", "ll", "photo5.jpg", 6),
-    ("ll", "ll", "photo6.jpg", 6),
-    ("ll", "ll", "photo7.jpg", 6),
-    ("ll", "ll", "photo8.jpg", 6),
-    ("ll", "ll", "photo9.jpg", 6),
-    ("ll", "ll", "photo10.jpg", 6),
-    ("ll", "ll", "photo1.jpg", 7),
-    ("ll", "ll", "photo2.jpg", 7),
-    ("ll", "ll", "photo3.jpg", 7),
-    ("ll", "ll", "photo4.jpg", 7),
-    ("ll", "ll", "photo5.jpg", 7),
-    ("ll", "ll", "photo6.jpg", 7),
-    ("ll", "ll", "photo7.jpg", 7),
-    ("ll", "ll", "photo8.jpg", 7),
-    ("ll", "ll", "photo9.jpg", 7),
-    ("ll", "ll", "photo10.jpg", 7),
-    ("ll", "ll", "photo1.jpg", 8),
-    ("ll", "ll", "photo2.jpg", 8),
-    ("ll", "ll", "photo3.jpg", 8),
-    ("ll", "ll", "photo4.jpg", 8),
-    ("ll", "ll", "photo5.jpg", 8),
-    ("ll", "ll", "photo6.jpg", 8),
-    ("ll", "ll", "photo7.jpg", 8),
-    ("ll", "ll", "photo8.jpg", 8),
-    ("ll", "ll", "photo9.jpg", 8),
-    ("ll", "ll", "photo10.jpg", 8),
-    ("ll", "ll", "photo1.jpg", 9),
-    ("ll", "ll", "photo2.jpg", 9),
-    ("ll", "ll", "photo3.jpg", 9),
-    ("ll", "ll", "photo4.jpg", 9),
-    ("ll", "ll", "photo5.jpg", 9),
-    ("ll", "ll", "photo6.jpg", 9),
-    ("ll", "ll", "photo7.jpg", 9),
-    ("ll", "ll", "photo8.jpg", 9),
-    ("ll", "ll", "photo9.jpg", 9),
-    ("ll", "ll", "photo10.jpg", 9),
-    ("ll", "ll", "photo1.jpg", 10),
-    ("ll", "ll", "photo2.jpg", 10),
-    ("ll", "ll", "photo3.jpg", 10),
-    ("ll", "ll", "photo4.jpg", 10),
-    ("ll", "ll", "photo5.jpg", 10),
-    ("ll", "ll", "photo6.jpg", 10),
-    ("ll", "ll", "photo7.jpg", 10),
-    ("ll", "ll", "photo8.jpg", 10),
-    ("ll", "ll", "photo9.jpg", 10),
-    ("ll", "ll", "photo10.jpg", 10);
+VALUES (
+        "ll",
+        "ll",
+        "images/photos/photographer1/photo1.jpg",
+        1
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer1/photo2.jpg",
+        1
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer1/photo3.jpg",
+        1
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer1/photo4.jpg",
+        1
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer1/photo5.jpg",
+        1
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer1/photo6.jpg",
+        1
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer1/photo7.jpg",
+        1
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer1/photo8.jpg",
+        1
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer1/photo9.jpg",
+        1
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer1/photo10.jpg",
+        1
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer2/photo1.jpg",
+        2
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer2/photo2.jpg",
+        2
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer2/photo3.jpg",
+        2
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer2/photo4.jpg",
+        2
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer2/photo5.jpg",
+        2
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer2/photo6.jpg",
+        2
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer2/photo7.jpg",
+        2
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer2/photo8.jpg",
+        2
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer2/photo9.jpg",
+        2
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer2/photo10.jpg",
+        2
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer3/photo1.jpg",
+        3
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer3/photo2.jpg",
+        3
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer3/photo3.jpg",
+        3
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer3/photo4.jpg",
+        3
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer3/photo5.jpg",
+        3
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer3/photo6.jpg",
+        3
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer3/photo7.jpg",
+        3
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer3/photo8.jpg",
+        3
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer3/photo9.jpg",
+        3
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer3/photo10.jpg",
+        3
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo1.jpg",
+        4
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo2.jpg",
+        4
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo3.jpg",
+        4
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo4.jpg",
+        4
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo5.jpg",
+        4
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo6.jpg",
+        4
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo7.jpg",
+        4
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo8.jpg",
+        4
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo9.jpg",
+        4
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo10.jpg",
+        4
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer5/photo1.jpg",
+        5
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer5/photo2.jpg",
+        5
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer5/photo3.jpg",
+        5
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer5/photo4.jpg",
+        5
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer5/photo5.jpg",
+        5
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer5/photo6.jpg",
+        5
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer5/photo7.jpg",
+        5
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer5/photo8.jpg",
+        5
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer5/photo9.jpg",
+        5
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer5/photo10.jpg",
+        5
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer6/photo1.jpg",
+        6
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer6/photo2.jpg",
+        6
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer6/photo3.jpg",
+        6
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer6/photo4.jpg",
+        6
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer6/photo5.jpg",
+        6
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer6/photo6.jpg",
+        6
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer6/photo7.jpg",
+        6
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer6/photo8.jpg",
+        6
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer6/photo9.jpg",
+        6
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer6/photo10.jpg",
+        6
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer7/photo1.jpg",
+        7
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer4/photo2.jpg",
+        7
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer7/photo3.jpg",
+        7
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer7/photo4.jpg",
+        7
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer7/photo5.jpg",
+        7
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer7/photo6.jpg",
+        7
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer7/photo7.jpg",
+        7
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer7/photo8.jpg",
+        7
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer7/photo9.jpg",
+        7
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer7/photo10.jpg",
+        7
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer8/photo1.jpg",
+        8
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer8/photo2.jpg",
+        8
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer8/photo3.jpg",
+        8
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer8/photo4.jpg",
+        8
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer8/photo5.jpg",
+        8
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer8/photo6.jpg",
+        8
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer8/photo7.jpg",
+        8
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer8/photo8.jpg",
+        8
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer8/photo9.jpg",
+        8
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer8/photo10.jpg",
+        8
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer9/photo1.jpg",
+        9
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer9/photo2.jpg",
+        9
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer9/photo3.jpg",
+        9
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer9/photo4.jpg",
+        9
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer9/photo5.jpg",
+        9
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer9/photo6.jpg",
+        9
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer9/photo7.jpg",
+        9
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer9/photo8.jpg",
+        9
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer9/photo9.jpg",
+        9
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer9/photo10.jpg",
+        9
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer10/photo1.jpg",
+        10
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer10/photo2.jpg",
+        10
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer10/photo3.jpg",
+        10
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer10/photo4.jpg",
+        10
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer10/photo5.jpg",
+        10
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer10/photo6.jpg",
+        10
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer10/photo7.jpg",
+        10
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer10/photo8.jpg",
+        10
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer10/photo9.jpg",
+        10
+    ),
+    (
+        "ll",
+        "ll",
+        "images/photos/photographer10/photo10.jpg",
+        10
+    );
 
 CREATE TABLE gallery (
     id_user INT REFERENCES user (id),
@@ -213,7 +712,6 @@ CREATE TABLE user (
     CONSTRAINT UX_email UNIQUE (email)
 );
 
-
 INSERT INTO
     user (
         firstname,
@@ -222,14 +720,10 @@ INSERT INTO
         hashed_password,
         is_admin
     )
-VALUES(
-    'Hakim',
-    'AZIZI',
-    'hakazizi@hotmail.com',
-    '$argon2id$v=19$m=19456,t=2,p=1$w6uJTyInKVc1CtIzy8XScw$3VHtmwsXu7xyxJQh6MznrB+w4yrGHRTEe0utyPOpO0Q',
-    1
-)
-
-
-
-
+VALUES (
+        'Hakim',
+        'AZIZI',
+        'hakazizi@hotmail.com',
+        '$argon2id$v=19$m=19456,t=2,p=1$w6uJTyInKVc1CtIzy8XScw$3VHtmwsXu7xyxJQh6MznrB+w4yrGHRTEe0utyPOpO0Q',
+        1
+    )
